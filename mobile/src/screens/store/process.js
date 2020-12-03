070eecd33809d3922930d112e130b2d9ad4563a2
pvthanh98 <pvthanh98it@gmail.com>
2020-12-03 14:15:03 +0700

    complete everything

--- mobile/src/screens/store/process.js
+++ mobile/src/screens/store/process.js
@@ -6,9 +6,17 @@ import ProcessModal from '../../components/admin_side/process/modal';
 import axios from '../../service/store_axios';
 
 export default function Process(props) {
-  const {rescue_id, customer_id, phone, address, name,coordinate} = props.route.params;
+  const {
+    rescue_id,
+    customer_id,
+    phone,
+    address,
+    name,
+    coordinate,
+  } = props.route.params;
   const [visible, setVisible] = React.useState(false);
   const [services, setServices] = React.useState([]);
+  const [isSuccess, setIsSuccess] = React.useState(false);
   console.log(rescue_id);
   const renderServices = () =>
     services.map((e) => (
@@ -70,77 +78,96 @@ export default function Process(props) {
       total_cost,
       services: service_bill,
       coordinate,
-      rescue_id
+      rescue_id,
     };
 
     axios
       .post('/api/bill', bill)
       .then((reslt) => {
-        alert("Thành Công");
+        setIsSuccess(true)
       })
       .catch((err) => console.log(err));
   };
   return (
-    <View style={styles.container}>
-      <View style={{flex: 1}}>
-        <Title style={styles.title}>Thông tin khách hàng</Title>
-        <View style={styles.itemRow}>
-          <Text style={styles.leftItem}>Tên khách hàng</Text>
-          <Text style={styles.rightItem}>{name}</Text>
-        </View>
-        <View style={styles.itemRow}>
-          <Text style={styles.leftItem}>Số điện thoại</Text>
-          <Text style={styles.rightItem}>{phone}</Text>
-        </View>
-        <View style={styles.itemRow}>
-          <Text style={styles.leftItem}>Địa chỉ</Text>
-          <Text style={styles.rightItem}>{address}</Text>
-        </View>
-      </View>
-      <View style={{flex: 2, justifyContent: 'space-between'}}>
-        <ScrollView>
-          <Title style={styles.title}>Dịch vụ</Title>
+    <View
+      style={[
+        styles.container,
+        {justifyContent: isSuccess ? 'center' : 'flex-start'},
+      ]}>
+      {!isSuccess ? <View style={{flex:1}}>
+        <View style={{flex: 1}}>
+          <Title style={styles.title}>Thông tin khách hàng</Title>
           <View style={styles.itemRow}>
-            <Text style={[styles.leftItem, {flex: 2, fontWeight: 'bold'}]}>
-              Tên Dịch Vụ
-            </Text>
-            <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
-              Đơn Giá
-            </Text>
-            <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
-              Số lượng
-            </Text>
-            <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
-              <IconButton
-                icon={() => (
-                  <MaterialIcon name="add-box" color="green" size={20} />
-                )}
-                onPress={() => setVisible(true)}
-              />
-            </Text>
+            <Text style={styles.leftItem}>Tên khách hàng</Text>
+            <Text style={styles.rightItem}>{name}</Text>
           </View>
-          <View>
-            {renderServices()}
+          <View style={styles.itemRow}>
+            <Text style={styles.leftItem}>Số điện thoại</Text>
+            <Text style={styles.rightItem}>{phone}</Text>
+          </View>
+          <View style={styles.itemRow}>
+            <Text style={styles.leftItem}>Địa chỉ</Text>
+            <Text style={styles.rightItem}>{address}</Text>
+          </View>
+        </View>
+        <View style={{flex: 2, justifyContent: 'space-between'}}>
+          <ScrollView>
+            <Title style={styles.title}>Dịch vụ</Title>
             <View style={styles.itemRow}>
-              <Text style={{paddingTop: 16, paddingBottom: 16}}>
-                Tổng:{' '}
-                <Text style={{color: 'red', fontWeight: 'bold'}}>
-                  $ {calculateTotalCost()}
-                </Text>
+              <Text style={[styles.leftItem, {flex: 2, fontWeight: 'bold'}]}>
+                Tên Dịch Vụ
+              </Text>
+              <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
+                Đơn Giá
+              </Text>
+              <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
+                Số lượng
+              </Text>
+              <Text style={[styles.rightItem, {flex: 1, fontWeight: 'bold'}]}>
+                <IconButton
+                  icon={() => (
+                    <MaterialIcon name="add-box" color="green" size={20} />
+                  )}
+                  onPress={() => setVisible(true)}
+                />
               </Text>
             </View>
-          </View>
-        </ScrollView>
-        <Button mode="contained" onPress={onSubmitBill} color="green">
-          Thêm hóa đơn và đánh dấu hoàn tất
-        </Button>
+            <View>
+              {renderServices()}
+              <View style={styles.itemRow}>
+                <Text style={{paddingTop: 16, paddingBottom: 16}}>
+                  Tổng:{' '}
+                  <Text style={{color: 'red', fontWeight: 'bold'}}>
+                    $ {calculateTotalCost()}
+                  </Text>
+                </Text>
+              </View>
+            </View>
+          </ScrollView>
+          <Button mode="contained" onPress={onSubmitBill} color="green">
+            Thêm hóa đơn và đánh dấu hoàn tất
+          </Button>
+        </View>
+        <ProcessModal
+          visible={visible}
+          setVisible={setVisible}
+          services={services}
+          setServices={setServices}
+        />
       </View>
-      <ProcessModal
-        visible={visible}
-        setVisible={setVisible}
-        services={services}
-        setServices={setServices}
-      />
+      : <View style={{alignItems:"center"}}>
+          <MaterialIcon name="check-circle" size={140} color="green" />
+          <Text style={{fontSize:24}}>Lập hóa đơn thành công</Text>
+          <Button 
+            mode="contained" 
+            color="#e0e0e0"
+            icon={()=> <MaterialIcon name="arrow-back-ios" size={16} color="black" />}
+            onPress={()=>props.navigation.goBack()}
+          >
+            Trở về
+          </Button>
+        </View>  
+    }
     </View>
   );
 }
